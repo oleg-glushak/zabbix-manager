7be54dc39ed2eeea60dc78ebb42780fcfffb6f73
Oleg Glushak <o.glushak@mocomedia.ru>
2016-01-13 17:40:19 +0300

    Initial commit

---
+++ zabbix-manager.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+
+import os
+import datetime
+import logging
+import re
+import sys
+import argparse
+from operator import itemgetter
+
+def main():
+    print "Hello"
+
+if __name__ == '__main__':
+    main()
