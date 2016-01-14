e1f85e96de7e0e52bc7e96fafc67f382fc3f5fae
Oleg Glushak <o.glushak@mocomedia.ru>
2016-01-14 11:17:57 +0300

    test pycharm

--- zabbix-manager.py
+++ zabbix-manager.py
@@ -1,16 +1,31 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#!/usr/bin/env python
-
-import os
-import datetime
+#import os
+#import datetime
 import logging
-import re
-import sys
-import argparse
-from operator import itemgetter
+import platform
+#import re
+#import sys
+#from operator import itemgetter
+
+zabbix_api_uri = "http://zabbix.mocomedia.ru/api_jsonrpc.php"
+zabbix_user = "api"
+zabbix_password = "SecretPassword"
+foreman_api_uri = "http://xen-puppet1.local.dgvg.ru/api"
+foreman_user = "api"
+foreman_password = "SecretPassword"
+log_file = "/var/log/zabbix-manager.log"
+
+def get_my_hostname():
+    return platform.node()
+
+def get_my_hostgroup():
+    #get my hostname and get hostgroup for my hostname
+    return "testhostgroup"
 
 def main():
+    hostgroup = get_my_hostgroup()
     print "Hello"
 
 if __name__ == '__main__':
